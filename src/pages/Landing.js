d86e75ce450de8dfca3c086f559cb5ec4b918e50
Henry Feng <henryfeng12345@gmail.com>
2021-01-14 11:46:09 -0600

    add arrow animation

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,14 +1,20 @@
 import "./Landing.css";
-import me from "./me.jpg";
+
+import me from "./media/me.jpg";
+import rightArrow from "./media/right-arrow.svg";
+
 export default function Landing() {
   return (
     <div>
       <div className="parent">
-        <h1 className="child">Henry Feng</h1>
+        <h1 className="">Henry Feng</h1>
       </div>
       <div>
         <div className="photoContainer">
           <img className="photo" src={me} />
+        </div>
+        <div className="parent">
+          <img className="child rightArrow" src={rightArrow} alt="React Logo" />
         </div>
       </div>
     </div>
