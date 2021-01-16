1bc9e81f791215f56b517d3363a5d647289e516d
Henry Feng <henryfeng12345@gmail.com>
2021-01-16 14:12:00 -0600

    add smooth scroll and linkedin icon

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         </Route>
         <Route path="/">
           <Landing />
-          <Content />
         </Route>
       </Switch>
     </Router>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,17 +2,44 @@ import "./Landing.css";
 
 import me from "./media/me.jpg";
 import rightArrow from "./media/right-arrow.svg";
+import linkedinIcon from "./media/logo-linkedin.svg";
+
+import Content from "./Content";
 
 export default function Landing() {
   return (
-    <div className="parent">
-      <span className="title">Henry Feng</span>
-      <div className="photoContainer">
-        <img className="photo" src={me} />
-      </div>
-      <div className="">
-        <img className="child rightArrow" src={rightArrow} alt="React Logo" />
-      </div>
-    </div>
+    <>
+      <section>
+        <div className="parent">
+          <span className="title">Henry Feng</span>
+          <div className="photoContainer">
+            <img className="photo" src={me} />
+
+            <div className="socials">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/henry-feng-9a11b0129/"
+              >
+                <img className="photoSocials" src={linkedinIcon} />
+              </a>
+            </div>
+          </div>
+          <div className="">
+            <a href="#content">
+              <img
+                className="child rightArrow"
+                href="#content"
+                src={rightArrow}
+                alt="React Logo"
+              />
+            </a>
+          </div>
+        </div>
+      </section>
+      <section id="content">
+        <Content id="content" />
+      </section>
+    </>
   );
 }
